47e46113ff9e830d88e9b7640a976cb67689b16b
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-01-22 01:28:58 +0800

    feat: variants examples

---
+++ src/data/variants.ml
@@ -0,0 +1,35 @@
+type primary_color =
+  | Red
+  | Green
+  | Blue
+
+let r = Red
+
+type point = float * float
+
+(* enum! *)
+type shapes =
+  | Circle of
+      { center : point
+      ; radius : float
+      }
+  | Rectangle of
+      { lower_left : point
+      ; upper_right : point
+      }
+
+let c1 = Circle { center = 0., 0.; radius = 1. }
+let r1 = Rectangle { lower_left = -1., -1.; upper_right = 1., 1. }
+let avg a b = (a +. b) /. 2.
+
+let center s =
+  match s with
+  | Circle { center; _ } -> center
+  | Rectangle { lower_left; upper_right } ->
+    let x_ll, y_ll = lower_left in
+    let x_ur, y_ur = upper_right in
+    avg x_ll x_ur, avg y_ll y_ur
+;;
+
+let%test _ = center c1 = (0., 0.)
+let%test _ = center r1 = (0., 0.)
