1442b1331dc9a58ca6676b1926ba4cfc661c5d1e
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-01-21 20:08:50 +0800

    feat: application operators

---
+++ application_ops.ml
@@ -0,0 +1,23 @@
+(* Application *)
+(* Take function f and param x, apply f to x, that is f(x) *)
+let (@@) f x = f x;;
+
+(* Reverse Application a.k.a pipeline *)
+let (|>) x f = f x;
+
+succ 2 * 10;;
+(* which is equiv to *)
+(succ 2) * 10;;
+(* what if I want to mean succ (2 * 10) *)
+succ @@ 2 * 10;;
+
+(* Example: square *)
+let square x = x * x;;
+(* calculate ((5 + 1)^2)^2 + 1 *)
+succ ( square ( square (succ 5) ) );;
+(* uh.. ugly! using pipeline! *)
+5
+|> succ
+|> square
+|> square
+|> succ ;; (* beautiful! *)
