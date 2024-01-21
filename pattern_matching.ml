96e1679297bed735778fad6bd8756ce238c8dfde
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-01-21 21:12:15 +0800

    feat: more pattern matching against list

--- pattern_matching.ml
+++ pattern_matching.ml
@@ -21,3 +21,29 @@ let b =
   match ["taylor"; "swift"] with
   | [] -> "folklore"
   | h :: t -> h
+
+(* check if list is empty *)
+let empty lst =
+  match lst with
+  | [] -> true
+  | h(*head*) :: t(*tail*) -> false
+
+let rec sum lst =
+  match lst with
+  | [] -> 0
+  | h :: t -> h + sum t
+(* try #trace sum in utop to check the recursive call *)
+
+let rec length lst =
+  match lst with
+  | [] -> 0
+  | h :: t -> 1 + length t
+
+
+(* example useage:
+  append [1; 2; 3] [4; 5; 6] is [1; 2; 3; 4; 5; 6]
+*)
+let rec append lst1 lst2 =
+  match lst1 with
+  | [] -> lst2
+  | h :: t -> h :: (append t lst2)
