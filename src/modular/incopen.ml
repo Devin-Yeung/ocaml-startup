f7d7f60f97a67811532f9a29f441015a983fbb74
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-01-25 18:22:59 +0800

    feat: include vs open

---
+++ src/modular/incopen.ml
@@ -0,0 +1,26 @@
+module M = struct
+  let x = 0
+end
+
+(* 
+  sig
+  val x: int
+  val y: int
+  end
+*)
+module N = struct
+  include M
+
+  let y = x + 1
+end
+
+(* 
+  sig
+  val y: int
+  end
+*)
+module O = struct
+  open M
+
+  let y = x + 1
+end
